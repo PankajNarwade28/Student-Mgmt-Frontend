9ec47d8174ae0aded55e209cf0a0bd9260599bbf
PankajNarwade28 <pankajnarwade258@gmail.com>
2026-01-23 12:42:04 +0530

    Adjusted Route for Tabs

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -7,8 +6,9 @@ import { Toaster } from "react-hot-toast";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
-  <React.StrictMode>
+ 
+  <>
     <Toaster position="top-right" />
     <App />
-  </React.StrictMode>,
+    </> 
 );
